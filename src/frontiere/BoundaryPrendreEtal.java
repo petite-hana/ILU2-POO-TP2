d07689f9fd4a04a4708c90e3d8acce45247c9180
Hana <hana.descazeaux@univ-tls3.fr>
2025-10-15 11:48:15 +0200

    Implementation du cas "Prendre Etal"

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -10,10 +10,28 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.printf("Je suis désolé %s mais il faut être un habitant de notre village pour commercer ici.\n", nomVendeur);
+			return;
+		}
+
+		System.out.printf("Bonjour %s, je vais regarder si je peux vous trouver un étal.\n", nomVendeur);
+		if (!controlPrendreEtal.resteEtals()) {
+			System.out.printf("Désolée %s, je n'ai plus d'étal qui ne soit pas déjà occupé.\n", nomVendeur);
+			return;
+		}
+
+		installerVendeur(nomVendeur);
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait, il me reste un étal pour vous !\nIl me faudrait quelques renseignements :");
+		String produit = Clavier.entrerChaine("Quel produit souhaitez-vous vendre ?");
+		int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre ?");
+
+		int etal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		if (etal != -1) {
+			System.out.printf("Le vendeur %s s'est installé à l'étal n°%d\n", nomVendeur, etal + 1);
+		}
 	}
 }
