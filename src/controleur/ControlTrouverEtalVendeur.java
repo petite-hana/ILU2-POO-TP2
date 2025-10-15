5e4ea176ad65357be9bbde6b462687a8a8553128
Hana <hana.descazeaux@univ-tls3.fr>
2025-10-15 11:52:41 +0200

    Implementation du cas "Trouver Etal Vendeur"

--- src/controleur/ControlTrouverEtalVendeur.java
+++ src/controleur/ControlTrouverEtalVendeur.java
@@ -1,5 +1,6 @@
 package controleur;
 
+import personnages.Gaulois;
 import villagegaulois.Etal;
 import villagegaulois.Village;
 
@@ -11,8 +12,7 @@ public class ControlTrouverEtalVendeur {
 	}
 
 	public Etal trouverEtalVendeur(String nomVendeur) {
-		//TODO a completer
-		Etal etal = null;
-		return etal;
+		Gaulois gaulois = village.trouverHabitant(nomVendeur);
+		return gaulois != null ? village.rechercherEtal(gaulois) : null;
 	}
 }
